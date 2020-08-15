f0a57ece425a28eb4e9075fc3b5560ad93944f82
DESKTOP-30RJMTC\kimo8 <kimo87@gmail.com>
2020-08-15 16:28:49 +0900

    트윗정보 올리기

--- 깔끔한 파이썬 탄탄한 백엔드/API/app.py
+++ 깔끔한 파이썬 탄탄한 백엔드/API/app.py
@@ -3,6 +3,27 @@ from flask import Flask,jsonify,request
 app= Flask(__name__)
 app.users = {}
 app.id_count =1
+app.tweets=[]
+
+def tweet():
+    payload=request.josn
+    user_id=int(payload['id'])
+    tweet =payload['tweet']
+
+    if user_id not in app.users:
+        return  '사용자가 존재하지 않습니다.'
+
+    if len(tweet)>300:
+        return '300자를 초과했습니다.',400
+
+    user_id=int(payload['id'])
+    app.tweets.append(
+        {
+            'user_id':user_id,
+            'tweet'  : tweet
+        }
+    )
+    return '',200
 
 @app.route("/sign-up",methods=['POST'])
 def sign_up():
