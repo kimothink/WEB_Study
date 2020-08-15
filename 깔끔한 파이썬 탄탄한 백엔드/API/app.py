d63145b740ab85c2ae2e98a5fe7742ec8ee9f7e9
DESKTOP-30RJMTC\kimo8 <kimo87@gmail.com>
2020-08-15 17:15:42 +0900

    5장정리

--- 깔끔한 파이썬 탄탄한 백엔드/API/app.py
+++ 깔끔한 파이썬 탄탄한 백엔드/API/app.py
@@ -1,10 +1,48 @@
 from flask import Flask,jsonify,request
-
+from flask.json import JSONEncoder
 app= Flask(__name__)
 app.users = {}
 app.id_count =1
 app.tweets=[]
 
+class CustomJSONEncoder(JSONEncoder):
+    def default(self,obj):
+        if isinstance(obj,set):
+            return list(obj)
+
+        return JSONEncoder.default(self,obj)
+app.json_encoder =CustomJSONEncoder
+
+@app.route('/unfollow',methods=['POST'])
+def unfollow():
+    payload =request.json
+    user_id =int(payload['id'])
+    user_id_to_follow=int(payload['unfollow'])
+
+    if user_id not in app.users or user_id_to_follow not in app.users:
+        return '사용자가 존재하지 않습니다.',400
+    user=app.users[user_id]
+    user.setdefault('follow',set()).discard(user_id_to_follow)
+
+    return jsonify(user)
+
+@app.route('timeline/<int:user_id>',methods=['GET'])
+def timeline(user_id):
+    if user_id not in app.users:
+        return '사용자가 존재하지 않습니다',400
+
+    follow_list=app.users[user_id].get('follow',set())
+    follow_list.app(user_id)
+    timeline=[tweet for tweet in app.tweets if tweet['user_id'] in follow_list]
+
+    return jsonify(
+        {
+            'user_id':user_id,
+            'timeline':timeline
+        }
+    )
+
+@app.route('/tweet',methods=['POST'])
 def tweet():
     payload=request.josn
     user_id=int(payload['id'])
@@ -24,6 +62,21 @@ def tweet():
         }
     )
     return '',200
+
+@app.route('/follow',methods=['POST'])
+def follow():
+    payload =request.json
+    user_id=int(payload['id'])
+    user_id_to_follow=int(payload['follow'])
+
+    if user_id not in app.users or user_id_to_follow not in app.users:
+        return '사용자가 존재하지 않습니다.',400
+
+    user=app.users[user_id]
+    user.setdefault('follow',set()).add(user_id_to_follow)
+
+    return jsonify(user)
+
 
 @app.route("/sign-up",methods=['POST'])
 def sign_up():
