a80579763a41442e48f841d9edf85ce468793a23
DESKTOP-30RJMTC\kimo8 <kimo87@gmail.com>
2020-08-23 16:10:32 +0900

    오류 수정

--- Vue/67page/index.js
+++ Vue/67page/index.js
@@ -1,3 +1,4 @@
+
 new Vue({
   el: '#app',
   data: {
@@ -5,7 +6,7 @@ new Vue({
     list: []
   },
   created: function () {
-    axios.get('list.json').then(function (response) {
+    axios.get('./list.json').then(function (response) {
       // 데이터를 읽어 들였다면 list에 할당하기
       this.list = response.data
     }.bind(this)).catch(function (e) {
---
+++ Vue/86page/index.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: '#app',
+    data: {
+      show: true
+    },
+    methods: {
+      handleClick() {
+        var count = this.$refs.count
+        if (count) {
+          count.innerText = parseInt(count.innerText, 10) + 1
+        }
+      }
+    }
+  })
